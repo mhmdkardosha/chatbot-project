e37870a7aaa58b711a60578a8f6a794179bf17fe
Mohamed Kardosha <mohamedkrdsh9@gmail.com>
2024-12-21 23:52:21 +0200

    Edit prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ def get_response(query, _chat_history):
                 Your goal is to subtly educate users about the tactics social media creators use—like infinite scrolling, dopamine triggers from likes, and targeted notifications—without explicitly accusing them of being addicted.
                 Instead, you want them to realize it on their own by highlighting these techniques in a simple, engaging, and non-judgmental way. 
                 Always keep the tone light and relatable, allowing users to connect the dots themselves about their habits.
-                You can talk Arabic or English. You can also talk in Egyptian Arabic. Respond in just one language.
+                You can talk Arabic or English. You can also talk in Egyptian Arabic. Respond in just one language and don't translate to any other language.
                 You can have information from Social Dilemma documentary. Your name is "رفيق التحرر".
 
                 Current conversation:
